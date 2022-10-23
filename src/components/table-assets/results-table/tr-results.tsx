680113fa15dc45a6ff89eea1ad8c15b0b4677936
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-10-24 04:13:56 +0700

    ZO-84 Update tr-results (row of results table) and styles.

--- src/components/table-assets/results-table/tr-results.tsx
+++ src/components/table-assets/results-table/tr-results.tsx
@@ -1,3 +1,4 @@
+import { Component, createRef, ReactNode } from "react";
 import TS_ZAP from "../../../entities/ts-zap";
 import ZAP from "../../../entities/zap";
 import SUB_TABLEROW_Results from "./sub-tr-results";
@@ -9,38 +10,73 @@ export type TTABLEROW_Results_Props = {
         typeof TS_ZAP.fullName)[];
 }
 
-function TABLEROW_Results({ name, url, listScanType }: TTABLEROW_Results_Props) {
-    return (
-        <div className="trow-container">
-            <ul className="trow">
-                <li className="dropdown">
-                    <label className="dropdown-button button">
-                        <input type="checkbox" className="checkbox-input" />
-                        <div className="arrow">
-                        </div>
-                    </label>
-                </li>
-                <li className="name">
-                    {name}
-                </li>
-                <li className="target">
-                    {url}
-                </li>
-                <li className="scan-types">
-                    {listScanType.map((item) => {
-                        return <span className="scan-type-item">
-                            {item}
-                        </span>
-                    })}
-                </li>
-                <li className="action">
+type TTABLEROW_Results_State = {
+    isOpeningSubContent: boolean;
+}
 
-                </li>
-            </ul>
-            <div className="sub-trow-container">
+class TABLEROW_Results extends Component<TTABLEROW_Results_Props, TTABLEROW_Results_State> {
+    private ref_dropdownInput: React.RefObject<HTMLInputElement>;
+
+    constructor(props: TTABLEROW_Results_Props) {
+        super(props);
+        this.handleDropdownButton = this.handleDropdownButton.bind(this);
+        this.ref_dropdownInput = createRef<HTMLInputElement>();
+        this.state = {
+            isOpeningSubContent: false,
+        }
+    }
+
+    override componentDidMount(): void {
+        this.fetchIsOpeningSubContentState();
+    }
+
+    private handleDropdownButton() {
+        this.fetchIsOpeningSubContentState();
+    }
+
+    private fetchIsOpeningSubContentState(): void {
+        this.setState({
+            isOpeningSubContent: this.ref_dropdownInput.current?.checked ? true : false,
+        })
+    }
+
+    override render(): ReactNode {
+        return (
+            <div className="trow-container">
+                <ul className="trow">
+                    <li className="dropdown">
+                        <label className="dropdown-button button" onClick={this.handleDropdownButton}>
+                            <input type="checkbox" className="checkbox-input" ref={this.ref_dropdownInput} />
+                            <div className="arrow">
+                            </div>
+                        </label>
+                    </li>
+                    <li className="name">
+                        {this.props.name}
+                    </li>
+                    <li className="target">
+                        {this.props.url}
+                    </li>
+                    <li className="scan-types">
+                        {this.props.listScanType.map((item) => {
+                            return <span key={item} className="scan-type-item">
+                                {item}
+                            </span>
+                        })}
+                    </li>
+                    <li className="action">
+
+                    </li>
+                </ul>
+                <div className={`sub-trow-container ${this.state.isOpeningSubContent ? "is-opening" : ""}`}>
+                    <div className="sub-trow-wrap">
+                        <SUB_TABLEROW_Results scanType={ZAP.fullName} state={`SUCCEEDED`} progress={100} listExportResultType={["PDF", "XML"]} createdSince={"1 day ago"} />
+                        <SUB_TABLEROW_Results scanType={ZAP.fullName} state={`SUCCEEDED`} progress={100} listExportResultType={["PDF", "XML"]} createdSince={"1 day ago"} />
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
 
 export default TABLEROW_Results;
