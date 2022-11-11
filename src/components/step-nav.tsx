878ff5d8989cd74c79806be15aab79b88977d6b7
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-11-12 03:52:17 +0700

    ZO-90 Remove redundant lines in component and its style.

--- src/components/step-nav.tsx
+++ src/components/step-nav.tsx
@@ -1,5 +1,4 @@
 import { Component, createRef } from 'react';
-import { Link } from 'react-router-dom';
 import StepNode from './subs/navs/step-node';
 import { TStepNodeProps } from "./subs/navs/step-node";
 
@@ -17,12 +16,11 @@ class StepNav extends Component<TStepNavProps, TStepNavState> {
         super(props);
         this.ref_self = createRef<HTMLDivElement>();
     }
-    
+
     override componentDidMount(): void {
         const stepLength = this.props.steps.length;
         const marginValue = 100 / (stepLength * 2);
         this.ref_self.current?.style.setProperty("--linker-margin-left-right", `${marginValue}%`);
-        //     selfWidth: this.ref_self.current!.clientWidth,
     }
 
     override render() {
