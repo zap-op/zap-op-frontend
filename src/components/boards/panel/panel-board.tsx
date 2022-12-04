88d1b7d1a047e60f70846781b6be07af10fb0152
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-05 03:14:04 +0700

    ZO-136 Update refactor panel-board component.

--- src/components/boards/panel/panel-board.tsx
+++ src/components/boards/panel/panel-board.tsx
@@ -1,4 +1,4 @@
-import { Component, createRef, ReactNode } from "react";
+import { useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import workSpaceImage from "../../../assets/work-space.svg";
 
@@ -6,128 +6,116 @@ type TPanelDashboardProps = {
 
 }
 
-type TPanelDashboardState = {
-}
+const PanelDashboard = (props: TPanelDashboardProps) => {
+    const ref_panel = useRef<HTMLDivElement>(null);
 
-class PanelDashboard extends Component<TPanelDashboardProps, TPanelDashboardState> {
-    private ref_panel: React.RefObject<HTMLDivElement>;
-    private readonly LOCAL_STORAGE_KEY_PANEL_WIDTH = `UI.${PanelDashboard.name}.style.width`;
-    private readonly PANEL_WIDTH_DEFAULT = 300;
-
-    constructor(props: TPanelDashboardProps) {
-        super(props);
-        this.handleResizePanelOnMouseDown = this.handleResizePanelOnMouseDown.bind(this);
-        this.handleResizePanelOnMouseMove = this.handleResizePanelOnMouseMove.bind(this);
-        this.handleResizePanelOnMouseUp = this.handleResizePanelOnMouseUp.bind(this);
-        this.ref_panel = createRef<HTMLDivElement>();
-    }
-
-    override componentDidMount(): void {
-        const panelWidth = localStorage.getItem(this.LOCAL_STORAGE_KEY_PANEL_WIDTH);
+    useEffect(() => {
+        const panelWidth = localStorage.getItem(PanelDashboard.LOCAL_STORAGE_KEY_PANEL_WIDTH);
         if (!panelWidth) {
-            this.resizePanel(`${this.PANEL_WIDTH_DEFAULT}px`);
+            resizePanel(`${PanelDashboard.PANEL_WIDTH_DEFAULT}px`);
         } else {
-            this.resizePanel(`${panelWidth}px`);
+            resizePanel(`${panelWidth}px`);
         }
+    }, [])
+
+    const resizePanel = (width: string): void => {
+        ref_panel.current!.style.width = width;
     }
 
-    private resizePanel(width: string): void {
-        this.ref_panel.current!.style.width = width;
+    const getCurentPanelWidth = (): number => {
+        return ref_panel.current!.clientWidth;
     }
 
-    private getCurentPanelWidth(): number {
-        return this.ref_panel.current!.clientWidth;
+    const handleResizePanelOnMouseMove = (event: MouseEvent) => {
+        resizePanel(`${getCurentPanelWidth() + event.movementX}px`);
+        localStorage.setItem(PanelDashboard.LOCAL_STORAGE_KEY_PANEL_WIDTH, `${getCurentPanelWidth()}`);
     }
 
-    private handleResizePanelOnMouseMove(event: MouseEvent) {
-        this.resizePanel(`${this.getCurentPanelWidth() + event.movementX}px`);
-        localStorage.setItem(this.LOCAL_STORAGE_KEY_PANEL_WIDTH, `${this.getCurentPanelWidth()}`);
+    const handleResizePanelOnMouseUp = () => {
+        document.removeEventListener("mousemove", handleResizePanelOnMouseMove);
+        document.removeEventListener("mouseup", handleResizePanelOnMouseUp);
     }
 
-    private handleResizePanelOnMouseUp() {
-        document.removeEventListener("mousemove", this.handleResizePanelOnMouseMove);
-        document.removeEventListener("mouseup", this.handleResizePanelOnMouseUp);
+    const handleResizePanelOnMouseDown = () => {
+        document.addEventListener("mousemove", handleResizePanelOnMouseMove);
+        document.addEventListener("mouseup", handleResizePanelOnMouseUp);
     }
 
-    private handleResizePanelOnMouseDown() {
-        document.addEventListener("mousemove", this.handleResizePanelOnMouseMove);
-        document.addEventListener("mouseup", this.handleResizePanelOnMouseUp);
-    }
-
-    override render(): ReactNode {
-        return (
-            <div className="panel-dashboard-container">
-                <div className="panel-container" ref={this.ref_panel}>
-                    <div className="work-space-info-container">
-                        <span className="icon-container">
-                            <img src={workSpaceImage} alt="work-space.svg" />
+    return (
+        <div className="panel-dashboard-container">
+            <div className="panel-container" ref={ref_panel}>
+                <div className="work-space-info-container">
+                    <span className="icon-container">
+                        <img src={workSpaceImage} alt="work-space.svg" />
+                    </span>
+                    <div className="title-container">
+                        <h4 className="title">
+                            Example
+                        </h4>
+                        <span className="sub-title">
+                            Sub example
                         </span>
-                        <div className="title-container">
-                            <h4 className="title">
-                                Example
-                            </h4>
-                            <span className="sub-title">
-                                Sub example
-                            </span>
-                        </div>
-                    </div>
-                    <div className="work-space-nav-container">
-                        <h4 className="nav-title management-title">
-                            MANAGEMENT
-                        </h4>
-                        <NavLink to="dashboard" className="nav-item dashboard-container" draggable={false}>
-                            <span className="icon">
-                            </span>
-                            <h4 className="title">
-                                Dashboard
-                            </h4>
-                        </NavLink>
-                        <NavLink to="targets" className="nav-item targets-container" draggable={false}>
-                            <span className="icon">
-                            </span>
-                            <h4 className="title">
-                                Targets
-                            </h4>
-                        </NavLink>
-                        <NavLink to="results" className="nav-item results-container" draggable={false}>
-                            <span className="icon">
-                            </span>
-                            <h4 className="title">
-                                Results
-                            </h4>
-                        </NavLink>
-                        <h4 className="nav-title aciton-title">
-                            ACTION
-                        </h4>
-                        <NavLink to="addscan" className="nav-item new-scan-container" draggable={false}>
-                            <span className="icon">
-                            </span>
-                            <h4 className="title">
-                                New scan
-                            </h4>
-                        </NavLink>
-                    </div>
-                    <div className="br">
-                    </div>
-                    <div className="work-space-nav-container">
-                        <NavLink to="wspsettings" className="nav-item settings-container" draggable={false}>
-                            <span className="icon">
-                            </span>
-                            <h4 className="title">
-                                Workspace settings
-                            </h4>
-                        </NavLink>
                     </div>
                 </div>
-                <div className="panel-resize-controller">
-                    <button className="resize-bar-button" onMouseDown={this.handleResizePanelOnMouseDown}>
-                        <span className="resize-bar">
+                <div className="work-space-nav-container">
+                    <h4 className="nav-title management-title">
+                        MANAGEMENT
+                    </h4>
+                    <NavLink to="dashboard" className="nav-item dashboard-container" draggable={false}>
+                        <span className="icon">
                         </span>
-                    </button>
+                        <h4 className="title">
+                            Dashboard
+                        </h4>
+                    </NavLink>
+                    <NavLink to="targets" className="nav-item targets-container" draggable={false}>
+                        <span className="icon">
+                        </span>
+                        <h4 className="title">
+                            Targets
+                        </h4>
+                    </NavLink>
+                    <NavLink to="results" className="nav-item results-container" draggable={false}>
+                        <span className="icon">
+                        </span>
+                        <h4 className="title">
+                            Results
+                        </h4>
+                    </NavLink>
+                    <h4 className="nav-title aciton-title">
+                        ACTION
+                    </h4>
+                    <NavLink to="addscan" className="nav-item new-scan-container" draggable={false}>
+                        <span className="icon">
+                        </span>
+                        <h4 className="title">
+                            New scan
+                        </h4>
+                    </NavLink>
+                </div>
+                <div className="br">
+                </div>
+                <div className="work-space-nav-container">
+                    <NavLink to="wspsettings" className="nav-item settings-container" draggable={false}>
+                        <span className="icon">
+                        </span>
+                        <h4 className="title">
+                            Workspace settings
+                        </h4>
+                    </NavLink>
                 </div>
             </div>
-        );
-    }
+            <div className="panel-resize-controller">
+                <button className="resize-bar-button" onMouseDown={handleResizePanelOnMouseDown}>
+                    <span className="resize-bar">
+                    </span>
+                </button>
+            </div>
+        </div>
+    );
 }
+
+PanelDashboard.LOCAL_STORAGE_KEY_PANEL_WIDTH = `UI.${PanelDashboard.name}.style.width`;
+PanelDashboard.PANEL_WIDTH_DEFAULT = 300;
 
 export default PanelDashboard;
