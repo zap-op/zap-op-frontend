967a474a88cfc010ce240e3b4b98b9daeffeace5
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-05 03:37:19 +0700

    ZO-136 Update refactor add-target-modal component.

--- src/components/modals/add-target/add-target-modal.tsx
+++ src/components/modals/add-target/add-target-modal.tsx
@@ -5,34 +5,28 @@ import AddIPModal from "../add-ip/add-ip-modal";
 
 type TAddTargetsModalProps = {}
 
-class AddTargetsModal extends Component<TAddTargetsModalProps> {
-    constructor(props: TAddTargetsModalProps) {
-        super(props);
-    }
-
-    override render(): ReactNode {
-        return (
-            <div className="add-target-modal-container">
-                <div className="title-container">
-                    <h3>
-                        Add new target
-                    </h3>
-                </div>
-                <div className="content-container">
-                    <Link to="" state={AddDomainModal.LOCATION_STATE} className="type-option domain">
-                        <h4 className="title">
-                            Add domain
-                        </h4>
-                    </Link>
-                    <Link to="" state={AddIPModal.LOCATION_STATE} className="type-option ip">
-                        <h4 className="title">
-                            Add IP
-                        </h4>
-                    </Link>
-                </div>
+const AddTargetsModal = (props: TAddTargetsModalProps) => {
+    return (
+        <div className="add-target-modal-container">
+            <div className="title-container">
+                <h3>
+                    Add new target
+                </h3>
             </div>
-        )
-    }
+            <div className="content-container">
+                <Link to="" state={AddDomainModal.LOCATION_STATE} className="type-option domain">
+                    <h4 className="title">
+                        Add domain
+                    </h4>
+                </Link>
+                <Link to="" state={AddIPModal.LOCATION_STATE} className="type-option ip">
+                    <h4 className="title">
+                        Add IP
+                    </h4>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default AddTargetsModal;
