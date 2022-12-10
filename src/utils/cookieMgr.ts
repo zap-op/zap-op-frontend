9c2327ccabbcb28eb839258a55259b12e263fccd
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-10 14:28:30 +0700

    ZO-155 Add tokenMgr and cookieMgr.

---
+++ src/utils/cookieMgr.ts
@@ -0,0 +1,13 @@
+// Copyright and references from https://gist.github.com/hunan-rostomyan/28e8702c1cecff41f7fe64345b76f2ca
+export function getCookie(name: string): string | null {
+    const nameLenPlus = (name.length + 1);
+    return document.cookie
+        .split(';')
+        .map(c => c.trim())
+        .filter(cookie => {
+            return cookie.substring(0, nameLenPlus) === `${name}=`;
+        })
+        .map(cookie => {
+            return decodeURIComponent(cookie.substring(nameLenPlus));
+        })[0] || null;
+}
