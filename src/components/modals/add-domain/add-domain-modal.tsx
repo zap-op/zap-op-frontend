d1ea5f374947ec364ed6a5306aede272ae3df577
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-10 17:36:57 +0700

    ZO-112 Add handle change value input in content-input-field component and update related.

--- src/components/modals/add-domain/add-domain-modal.tsx
+++ src/components/modals/add-domain/add-domain-modal.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { TTarget } from "../../../submodules/utility/model";
 import ContentInputField from "../../fields/content-input-field/content-input-field";
 import { TModalProps } from "../../toolkits/modal/modal-portal";
 
@@ -6,31 +8,33 @@ type TAddDomainModalProps = TModalProps & {
 }
 
 const AddDomainModal = (props: TAddDomainModalProps) => {
+    const [nameTarget, setNameTarget] = useState<TTarget["name"]>();
+    const [target, setTarget] = useState<TTarget["target"]>();
 
     return (
         <div className="add-domain-modal-container">
-                <div className="title-container">
-                    <h3>
-                        Add domain
-                    </h3>
+            <div className="title-container">
+                <h3>
+                    Add domain
+                </h3>
+            </div>
+            <div className="content-container">
+                <div className="field-container target-name-field">
+                    <ContentInputField type="text" placeHolder="Target name" title="Name your target" isRequired={true} handleChangeValue={setNameTarget} />
                 </div>
-                <div className="content-container">
-                    <div className="field-container target-name-field">
-                        <ContentInputField type="text" placeHolder="Target name" title="Name your target" isRequired={true} />
-                    </div>
-                    <div className="field-container domain-field">
-                        <ContentInputField type="text" placeHolder={AddDomainModal.PLACEHOLDER} title="Enter your domain" isRequired={true} />
-                    </div>
+                <div className="field-container domain-field">
+                    <ContentInputField type="text" placeHolder={AddDomainModal.PLACEHOLDER} title="Enter your domain" isRequired={true} handleChangeValue={setTarget} />
                 </div>
-                <div className="navigator-state-containter">
-                    <Link to="" state={undefined} className="back-state button secondary-button">
-                        Back
-                    </Link>
                     <div className="add button primary-button">
-                        Add
-                    </div>
+            </div>
+            <div className="navigator-state-containter">
+                <Link to="" state={undefined} className="back-state button secondary-button">
+                    Back
+                </Link>
+                    Add
                 </div>
             </div>
+        </div>
     )
 }
 
--- src/components/modals/add-ip/add-ip-modal.tsx
+++ src/components/modals/add-ip/add-ip-modal.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { TTarget } from "../../../submodules/utility/model";
 import ContentInputField from "../../fields/content-input-field/content-input-field";
 import { TModalProps } from "../../toolkits/modal/modal-portal";
 
@@ -6,6 +8,8 @@ type TAddIPModalProps = TModalProps & {
 }
 
 const AddIPModal = (props: TAddIPModalProps) => {
+    const [nameTarget, setNameTarget] = useState<TTarget["name"]>();
+    const [target, setTarget] = useState<TTarget["target"]>();
 
     return (
         <div className="add-ip-modal-container">
@@ -17,10 +21,10 @@ const AddIPModal = (props: TAddIPModalProps) => {
             <div className="content-container">
                 <div className="content-container">
                     <div className="field-container target-name-field">
-                        <ContentInputField type="text" placeHolder="Target name" title="Name your target" isRequired={true} />
+                        <ContentInputField type="text" placeHolder="Target name" title="Name your target" isRequired={true} handleChangeValue={setNameTarget} />
                     </div>
                     <div className="field-container ip-field">
-                        <ContentInputField type="text" placeHolder={AddIPModal.PLACEHOLDER} title="Enter your IP" isRequired={true} />
+                        <ContentInputField type="text" placeHolder={AddIPModal.PLACEHOLDER} title="Enter your IP" isRequired={true} handleChangeValue={setTarget} />
                     </div>
                 </div>
             </div>
