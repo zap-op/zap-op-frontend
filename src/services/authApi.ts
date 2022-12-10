7faa9e07f5c52ce9a06ca342f424df4be8d0fc34
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-11 03:25:05 +0700

    ZO-111 Update type in auth api RTKQ.

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { GoogleCredentialResponse } from "../components/auth-google-button/auth-google-button";
 import authSlice from "../store/slice/authSlice";
+import { TStatusResponse } from "../submodules/utility/status";
+import { TOKEN_TYPE } from "../submodules/utility/token";
 import api from "./api";
 
 const authApi = createApi({
@@ -10,13 +12,13 @@ const authApi = createApi({
         credentials: "include",
     }),
     endpoints: (builder) => ({
-        login: builder.mutation<{ statusCode: number, msg: string }, GoogleCredentialResponse["credential"]>({
+        login: builder.mutation<TStatusResponse, GoogleCredentialResponse["credential"]>({
             query: () => ({
                 url: "login",
                 method: "POST",
             }),
             onQueryStarted: (_arg) => {
-                document.cookie = `ggToken=${_arg}`;
+                document.cookie = `${TOKEN_TYPE.GOOGLE}=${_arg}`;
             },
         })
     }),
