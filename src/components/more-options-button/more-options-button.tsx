e9c96c29d1d1af7e53d60668c0df7321fa22eb1d
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-12 01:48:38 +0700

    ZO-158 Add styleMgr, more-options-button component and styles.

---
+++ src/components/more-options-button/more-options-button.tsx
@@ -0,0 +1,22 @@
+import { FONT_SIZE } from "../../utils/styleMgr";
+
+const MoreOptionsButton = () => {
+    return (
+        <div className="more-options-button">
+            <div className="three-dot">
+                {/* ellipsis-solid.svg */}
+                <svg
+                    height={`${1.25 * FONT_SIZE}px`}
+                    width={`${1.25 * FONT_SIZE}px`}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 448 512">
+                    {/* <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --> */}
+                    <path
+                        d="M120 256c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56zm160 0c0 30.9-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56zm104 56c-30.9 0-56-25.1-56-56s25.1-56 56-56s56 25.1 56 56s-25.1 56-56 56z" />
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default MoreOptionsButton;
---
+++ src/utils/styleMgr.ts
@@ -0,0 +1,7 @@
+const STYLE = {
+    FONT_SIZE: 16
+}
+
+export const {
+    FONT_SIZE,
+} = STYLE
