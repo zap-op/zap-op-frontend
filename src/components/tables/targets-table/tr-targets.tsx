80ef7f474460454672fd1d401b5be54b30ef91a6
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-14 03:52:41 +0700

    ZO-114 Update connect move to trash target api RTKQ to tr-target component.

--- src/components/tables/targets-table/tr-targets.tsx
+++ src/components/tables/targets-table/tr-targets.tsx
@@ -1,20 +1,51 @@
 import { toast } from "react-hot-toast";
+import { useMoveToTrashTargetMutation } from "../../../services/targetApi";
 import { TTarget } from "../../../submodules/utility/model";
+import { TStatusResponse } from "../../../submodules/utility/status";
 import MoreOptionsButton, { TOptionsItem } from "../../more-options-button/more-options-button";
 
 export type TTABLEROW_Targets_Props = Omit<TTarget, "userId">
 
 const TABLEROW_Targets = (props: TTABLEROW_Targets_Props) => {
-    const newScanOption : TOptionsItem = {
+    const newScanOption: TOptionsItem = {
         name: "New scan",
         handle: () => {
             toast.error("Under development");
         }
     }
+    const [moveToTrashTarget] = useMoveToTrashTargetMutation();
     const deleteOption: TOptionsItem = {
         name: "Delete",
         handle: () => {
-
+            const toastId = toast.loading("Moving target to trash");
+            if (!props._id) {
+                toast.error("Something went wrong")
+                return;
+            }
+            moveToTrashTarget(props._id)
+                .unwrap()
+                .then((result) => {
+                    if (result.statusCode > 0) {
+                        toast.success(result.msg, {
+                            id: toastId,
+                        });
+                    } else {
+                        toast.error(result.msg, {
+                            id: toastId,
+                        });
+                    }
+                })
+                .catch((error) => {
+                    if (!error.data) {
+                        toast.error("Something went wrong", {
+                            id: toastId,
+                        });
+                        return;
+                    }
+                    toast.error((error.data as TStatusResponse).msg, {
+                        id: toastId,
+                    });
+                })
         }
     }
     return (
