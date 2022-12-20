3be775c7deca72ed010b46aeaf3b036ea9681ef0
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-20 21:36:42 +0700

    Update baseURL api to secrets.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { AxiosInstance } from "axios";
 const api = axios.create({
-    baseURL: process.env["REACT_APP_BASE_URL"] || "http://localhost:8888"
+    baseURL: process.env["REACT_APP_BASE_URL"],
 })
 
 export type { AxiosInstance };
