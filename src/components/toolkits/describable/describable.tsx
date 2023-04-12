b60f5e18a1369bd6b8af341b7b4d8bad4680df09
Huy Pham <pham.giahuy.9041@gmail.com>
2023-04-12 23:37:32 +0700

    ZO-109 Update descriable flow and disable mouse event on describe popup.

--- src/components/toolkits/describable/describable.tsx
+++ src/components/toolkits/describable/describable.tsx
@@ -1,60 +1,55 @@
-import { createRef, PropsWithChildren, useState } from 'react';
-import DescribeElement from './describe-element';
-import { setDescribeElement } from "../../../store/slice/toolkit/describablePortalSlice";
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../../store/store';
+import { PropsWithChildren, useRef, useState } from "react";
+import { setDescribeInfo } from "../../../store/slice/toolkit/describablePortalSlice";
+import { useDispatch } from "react-redux";
 
 type TDescribableProps = {
-    dataTitle: string,
-}
+	dataTitle: string;
+};
 
 const Describable = (props: PropsWithChildren<TDescribableProps>) => {
-    const isDescribing = useSelector((state: RootState) => state.describablePortal.isDescribing);
+	const TIME_TO_DESCRIBABLE = 500;
 
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const [waiterMouseStop, setWaiterMouseStop] = useState<NodeJS.Timeout | undefined>(undefined);
+	const [waiterMouseStop, setWaiterMouseStop] = useState<number>();
 
-    const ref_self = createRef<HTMLDivElement>();
+	const ref_self = useRef<HTMLDivElement>(null);
 
-    const handleMouseEnter = () => {
-        ref_self.current?.addEventListener("mousemove", handleMouseMove);
-    }
+	const handleMouseStop = () => {
+		const presentOffsetTop = ref_self.current!.offsetTop + ref_self.current!.offsetHeight / 2;
+		const presentOffsetLeft = ref_self.current!.offsetLeft + ref_self.current!.offsetWidth / 2;
+		dispatch(
+			setDescribeInfo({
+				describeInfo: {
+					offset: {
+						top: presentOffsetTop,
+						left: presentOffsetLeft,
+					},
+					title: props.dataTitle,
+				},
+			}),
+		);
+	};
 
-    const handleMouseMove = () => {
-        const handleMouseStop = () => {
-            const presentOffsetTop = (ref_self.current!.offsetTop + ref_self.current!.offsetHeight / 2);
-            const presentOffsetLeft = (ref_self.current!.offsetLeft + ref_self.current!.offsetWidth / 2);
-            dispatch(setDescribeElement({
-                describeElement: <DescribeElement
-                    offsetTop={presentOffsetTop}
-                    offsetLeft={presentOffsetLeft} >
-                    {props.dataTitle}
-                </DescribeElement>
-            }));
-        }
+	const handleMouseEnter = () => {
+		clearTimeout(waiterMouseStop);
+		setWaiterMouseStop(setTimeout(handleMouseStop, TIME_TO_DESCRIBABLE));
+	};
 
-        if (isDescribing) {
-            return;
-        }
-        clearTimeout(waiterMouseStop);
-        setWaiterMouseStop(setTimeout(handleMouseStop, Describable.TIME_TO_DESCRIBABLE));
-    }
+	const handleMouseLeave = () => {
+		clearTimeout(waiterMouseStop);
+		dispatch(setDescribeInfo({ describeInfo: undefined }));
+	};
 
-
-    const handleMouseLeave = () => {
-        clearTimeout(waiterMouseStop);
-        ref_self.current?.removeEventListener("mousemove", handleMouseMove);
-        dispatch(setDescribeElement({ describeElement: undefined }));
-    }
-
-    return (
-        <div className="describable-wrap" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ref={ref_self}>
-            {props.children}
-        </div>
-    );
-}
-
-Describable.TIME_TO_DESCRIBABLE = 200;
+	return (
+		<div
+			className="describable-wrap"
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+			ref={ref_self}>
+			{props.children}
+		</div>
+	);
+};
 
 export default Describable;
