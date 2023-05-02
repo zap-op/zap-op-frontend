bca1d7d2f50a21e279d3d83fba2a1b64d82329dc
Huy Pham <pham.giahuy.9041@gmail.com>
2023-05-02 14:47:07 +0700

    Update prettier executed.

--- src/pages/login-page/index.tsx
+++ src/pages/login-page/index.tsx
@@ -1,32 +1,25 @@
 import AuthGoogleButton from "../../components/buttons/auth-google-button";
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store';
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
 
 const LogInPage = () => {
-    const errorMessage = useSelector((state: RootState) => state.auth.errorMessage);
+	const errorMessage = useSelector((state: RootState) => state.auth.errorMessage);
 
-    return (
-        <div className="log-in-container">
-            <h2 className="title">
-                Log in
-            </h2>
-            <h4 className="title">
-                Log in your account with
-            </h4>
-            <div className="options-container">
-                <div className="google-option">
-                    <AuthGoogleButton option={AuthGoogleButton.LOG_IN_BUTTON_TEXT} />
-                </div>
-                <div className="github-option">
-                </div>
-                <div className="error-message-container">
-                    <h4 className="error-message">
-                        {errorMessage ? errorMessage : undefined}
-                    </h4>
-                </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="log-in-container">
+			<h2 className="title">Log in</h2>
+			<h4 className="title">Log in your account with</h4>
+			<div className="options-container">
+				<div className="google-option">
+					<AuthGoogleButton option={AuthGoogleButton.LOG_IN_BUTTON_TEXT} />
+				</div>
+				<div className="github-option"></div>
+				<div className="error-message-container">
+					<h4 className="error-message">{errorMessage ? errorMessage : undefined}</h4>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default LogInPage;
