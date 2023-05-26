8ae120aa2b6e710cd6aaed37feaed6ead6c5e4b3
Huy Pham <pham.giahuy.9041@gmail.com>
2023-05-26 21:51:21 +0700

    Update squash add target modal.

--- src/components/TargetsBoard/AddTargetModal/index.tsx
+++ src/components/TargetsBoard/AddTargetModal/index.tsx
@@ -1,6 +1,9 @@
-import AddDomainModal from "./AddDomainModal";
-import AddIPModal from "./AddIPModal";
-import { createContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
+import toast from "react-hot-toast";
+import { ModalContext } from "../../toolkits/ModalPortal";
+import TextField from "../../Fields/TextField";
+import { useAddTargetMutation } from "../../../store";
+import { TStatusResponse, TTarget } from "../../../utils/types";
 
 type TStateModal = "ip" | "domain" | undefined;
 
@@ -38,11 +41,11 @@ const AddTargetsModal = () => {
 				</div>
 			) : stateModal === "ip" ? (
 				<AddTargetsModalContext.Provider value={{ goBack }}>
-					<AddIPModal />
+					<AddModal typeTarget="ip" />
 				</AddTargetsModalContext.Provider>
 			) : stateModal === "domain" ? (
 				<AddTargetsModalContext.Provider value={{ goBack }}>
-					<AddDomainModal />
+					<AddModal typeTarget="domain" />
 				</AddTargetsModalContext.Provider>
 			) : (
 				<></>
@@ -52,3 +55,130 @@ const AddTargetsModal = () => {
 };
 
 export default AddTargetsModal;
+
+type TAddModal = {
+	typeTarget: Required<TStateModal>;
+};
+
+const AddModal = ({ typeTarget }: TAddModal) => {
+	const { title, placeHolder } = useMemo<{
+		title: string;
+		placeHolder: string;
+	}>(() => {
+		if (typeTarget === "domain") {
+			return {
+				title: "domain",
+				placeHolder: "eg. mydomain.com",
+			};
+		} else if (typeTarget === "ip") {
+			return {
+				title: "ip",
+				placeHolder: "eg. 192.158.1.24",
+			};
+		}
+		return {
+			title: "",
+			placeHolder: "",
+		};
+	}, []);
+
+	const [addTarget] = useAddTargetMutation();
+
+	const addTargetsModalContext = useContext(AddTargetsModalContext);
+	const modalContext = useContext(ModalContext);
+
+	const [nameTarget, setNameTarget] = useState<TTarget["name"]>();
+	const [isDisplayErrorMessageNameTargetField, setIsDisplayErrorMessageNameTargetField] = useState<boolean>();
+	const [target, setTarget] = useState<TTarget["target"]>();
+	const [isDisplayErrorMessageTargetField, setIsDisplayErrorMessageTargetField] = useState<boolean>();
+
+	const handleAddTarget = async () => {
+		if (!nameTarget || !target) {
+			if (!nameTarget) {
+				setIsDisplayErrorMessageNameTargetField(true);
+			} else {
+				setIsDisplayErrorMessageNameTargetField(false);
+			}
+			if (!target) {
+				setIsDisplayErrorMessageTargetField(true);
+			} else {
+				setIsDisplayErrorMessageTargetField(false);
+			}
+			return;
+		}
+		const toastId = toast.loading("Adding target");
+		let newTarget: TTarget = {
+			name: nameTarget,
+			target: target,
+		};
+		modalContext?.handleOpenModal(false);
+		addTarget(newTarget)
+			.unwrap()
+			.then((result) => {
+				if (result.statusCode > 0) {
+					toast.success(`${nameTarget} ${result.msg}`, {
+						id: toastId,
+					});
+				} else {
+					toast.error(result.msg, {
+						id: toastId,
+					});
+				}
+			})
+			.catch((error) => {
+				if (!error.data) {
+					toast.error("Something went wrong", {
+						id: toastId,
+					});
+					return;
+				}
+				toast.error((error.data as TStatusResponse).msg, {
+					id: toastId,
+				});
+			});
+	};
+
+	return (
+		<div className="add-modal-container">
+			<div className="title-container">
+				<h3>{`Add ${title}`}</h3>
+			</div>
+			<div className="content-container">
+				<div className="field-container target-name-field">
+					<TextField
+						type="text"
+						placeHolder="Target name"
+						title="Name your target"
+						isRequired={true}
+						handleChangeValue={setNameTarget}
+						errorMessage="Name target is required"
+						isDisplayErrorMessage={isDisplayErrorMessageNameTargetField}
+					/>
+				</div>
+				<div className="field-container target-field">
+					<TextField
+						type="text"
+						placeHolder={placeHolder}
+						title={`Enter your ${title}`}
+						isRequired={true}
+						handleChangeValue={setTarget}
+						errorMessage="Target is required"
+						isDisplayErrorMessage={isDisplayErrorMessageTargetField}
+					/>
+				</div>
+			</div>
+			<div className="navigator-state-containter">
+				<div
+					className="back-state button secondary-button"
+					onClick={addTargetsModalContext?.goBack}>
+					Back
+				</div>
+				<div
+					className="add button primary-button"
+					onClick={handleAddTarget}>
+					Add
+				</div>
+			</div>
+		</div>
+	);
+};
